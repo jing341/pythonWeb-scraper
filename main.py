836e5a8d8da4893b7d7b27dc3b50a70d7ee1ef41
jiung <takio0116@gmail.com>
2021-02-05 23:20:04 +0900

    python types and spacing

---
+++ main.py
@@ -0,0 +1,14 @@
+a_string = "like python"
+a_number = 3
+a_float = 3.54
+a_boolean = True
+a_none = None
+
+ #type()은 데이터 타입을 확인할 수 있는 함수이다. 문자는 str, 정수는 int, 소수는 float, true 또는 false는 bool 이고 none 은 NoneType 라는 타입을 가진다.
+print(type(a_string))
+print(type(a_number))
+print(type(a_float))
+print(type(a_boolean))
+print(type(a_none))
+
+#python 변수 이름을 정할 때 띄어쓰기가 필요하다면 '_' 를 사용한다. 예를 들어 super_long_variable 처럼 말이다.
